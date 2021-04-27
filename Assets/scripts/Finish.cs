a7eb5bfe70507474bfcddadd8f04acb3e979a9ef
VLad <vladislav.komarov.06@mail.ru>
2021-04-27 21:33:19 +0800

    п

--- Assets/scripts/Finish.cs
+++ Assets/scripts/Finish.cs
@@ -7,13 +7,13 @@ public class Finish : MonoBehaviour
    [SerializeField]
     // Start is called before the first frame update
     public GameObject EndScreen;
-    GameObject obj;
+  
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             EndScreen.gameObject.SetActive(true);
-            obj.GetComponent<Sphere_control>().Speed = 0; 
+            
         }
     }
 }
